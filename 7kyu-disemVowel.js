fb758b1a1447ff6f0ee29d17f65f3eab0660df64
aryarefman <155049856+aryarefman@users.noreply.github.com>
2024-01-24 15:03:51 +0700

    update 3

---
+++ 7kyu-disemVowel.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/52fba66badcd10859f00097e/train/javascript
+
+// const disemvowel = str => str.replace(/[aiueo]/gi ,'');
+
+
+const disemvowel = str => str.replace(/[aiueo]/gi ,'');
+
+console.log(disemvowel('This website is for losers LOL'));
--- 7kyu-vowelCount.js
+++ 7kyu-vowelCount.js
@@ -1,1 +1,15 @@
 // https://www.codewars.com/kata/54ff3102c1bad923760001f3/train/javascript
+
+// const getCount = str => {
+//     const chars = str.split('');
+//     const vowels = ['a', 'i', 'u', 'e', 'o'];
+//     const count = chars.filter(char => vowels.includes(char)).length;
+//     return count;
+// }
+
+
+function getCount(str) {
+    return (str.match(/[aeiou]/ig) || []).length;
+}
+
+console.log(getCount('abracadabra'));
